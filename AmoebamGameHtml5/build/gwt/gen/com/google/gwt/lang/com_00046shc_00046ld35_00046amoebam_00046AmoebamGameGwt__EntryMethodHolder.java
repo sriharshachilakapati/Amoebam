be1ab93de0c4333b3b9bccb78457e6f95a9f250d
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-04-18 00:27:37 +0530

    Uploading HTML5 build

---
+++ AmoebamGameHtml5/build/gwt/gen/com/google/gwt/lang/com_00046shc_00046ld35_00046amoebam_00046AmoebamGameGwt__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046shc_00046ld35_00046amoebam_00046AmoebamGameGwt__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
